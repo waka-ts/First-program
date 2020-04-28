9bb3ed9514a9f7bcee4b9609d8bd9b8113f55a2b
Kohei <kohei@Kohei-Pro.local>
2020-04-28 10:23:00 +0900

    お気に入りの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// RandomWordの呼び出し
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Start Up Name Generater',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
+        primaryColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+// Stateの生成
+class RandomWords extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  RandomWordsState createState() =>  RandomWordsState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// Stateの中身
+class RandomWordsState extends State<RandomWords>{
+  //候補を記録
+  final List<WordPair> _suggestions = <WordPair>[];
+  //お気に入りを保存
+  final Set<WordPair> _saved = new Set<WordPair>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  //押下を保存するメンバを追加
+  void _pushSaved(){
+    //routeをNavigatorのStackにpushする
+    Navigator. of(context).push(
+      //新しいrouteのコンテンツをMaterialPageRouteのbuilderプロパティで構築する
+      //Build関数は匿名関数として記述する
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          //ListTileを生成
+          final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair){
+                return new ListTile(
+                  title: new Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          //ListTile間に横方向スペースの追加(1pixelのボーダー)
+          final List<Widget> divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles)
+             //tileをリストに変換
+              .toList();
+
+          //builderのreturn,新routeのUI
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            //ListTileを含んだListViewを表示する
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 
+  
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  //ScaffoldでUI生成　中身は_buildSuggestions
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Start Up Name Generater'),
+        // タイトルにアクションの追加(アクションはWidgetの配列をとる)
+        actions: <Widget>[
+          //iconはiconButtonのメンバ、IconはIconsはウィジェット、
+          //Icon(Icons.settings)でアイコンを使える
+          //listは3行列のアイコン、押下で_pushSavedが作動
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+        ]
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: _buildSuggestions(),
+    );
+  }
+
+  //ListView表示させる レイアウトはBuildLowウィジェットで
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      //リスト中の文字の配置
+      padding: const EdgeInsets.all(16.0),
+      //itemBuilderはbuilderプロパティ
+      //引数はBuildContextとi、iは1ずつ増えていく
+      itemBuilder: (context, i) {
+        //iが奇数なら水平線(DividerWidget)を返す
+        if (i. isOdd) return Divider();
+        //indexはiを2で割って整数で返す
+        final index = i ~/ 2;
+        //index>=候補郡の長さ、つまり単語郡の最後にきたら、、、
+        if (index >= _suggestions.length){
+        //さらに10個生成して候補に追加する
+        _suggestions.addAll(generateWordPairs().take(10));
+        }
+        //indexが与えられる度に、buildRowが呼ばれる
+        return _buildRow(_suggestions[index]);
+       }
+     );
+    }
+
+  //リストのレイアウト
+  Widget _buildRow(WordPair pair) {
+      // お気に入りがあるか確認 containsで値が入ってたらTrue
+      final bool alreadySaved = _saved.contains(pair);
+      //ListTileを返す。ListTileはListレイアウトを作るウィジェット
+      return ListTile(
+        title: Text(
+          //WordPairを記述する
+          pair.asPascalCase,
+          style: _biggerFont,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        //trailingはアイコンウィジェット
+        trailing: Icon(
+          //trueでハート(赤)、falseでアンハートアイコン(色抜き)
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        //tapされると呼ばれる
+        onTap: (){
+          //setStateで状態の更新をUIに反映させる
+          //すでにお気に入りならremove,そうじゃないならadd
+          setState(()   {
+            if(alreadySaved){
+               _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        }
+      );
   }
 }
